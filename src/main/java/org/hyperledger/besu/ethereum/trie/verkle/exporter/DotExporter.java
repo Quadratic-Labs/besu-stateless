a8b721f8222d7a74ab1133ad7d22e056640efd3b
Filip.Gumula <filgum0326@gmail.com>
2023-11-15 11:09:14 +0100

    Enhancment of exportToDotFile() method in DotExporter
    Signed-off-by: Neo <neotheprogramist@proton.me>

--- src/main/java/org/hyperledger/besu/ethereum/trie/verkle/exporter/DotExporter.java
+++ src/main/java/org/hyperledger/besu/ethereum/trie/verkle/exporter/DotExporter.java
@@ -3,11 +3,16 @@ package org.hyperledger.besu.ethereum.trie.verkle.exporter;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.FileSystemException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/**
- * Utility class to save DOT format VerkleTrie to a file.
- */
 public class DotExporter {
+
+    private static final Pattern FILE_EXTENSION_PATTERN = Pattern.compile("\\.(dot|gv)$");
 
     /**
      * Exports the VerkleTrie to DOT format and saves it to a file.
@@ -16,11 +21,40 @@ public class DotExporter {
      * @param filePath            The path where the DOT file will be saved.
      */
     public static void exportToDotFile(String verkleTrieDotString, String filePath) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            writer.write(verkleTrieDotString);
-        } catch (IOException e) {
+        try {
+            if (filePath == null || filePath.isEmpty()) {
+                // If filePath is not provided, create a default one with the .dot extension.
+                filePath = "defaultVerkleTrie.dot";
+            } else {
+                // Check if the provided filePath has a valid extension (.dot or .gv).
+                Matcher matcher = FILE_EXTENSION_PATTERN.matcher(filePath);
+                if (!matcher.find()) {
+                    System.err.println("Error: Invalid file extension. Use .dot or .gv extension.");
+                    return;
+                }
+            }
+
+            Path path = Paths.get(filePath);
+
+            // Create directories if they don't exist.
+            if (!path.getParent().toFile().exists()) {
+                path.getParent().toFile().mkdirs();
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(path.toString()))) {
+                writer.write(verkleTrieDotString);
+            }
+
+        } catch (AccessDeniedException e) {
+            System.err.println("Error writing DOT file: Access denied. Check write permissions for the file.");
             e.printStackTrace();
+        } catch (FileSystemException e) {
+            System.err.println("Error writing DOT file: File system issue. Check disk space and file system restrictions.");
+            e.printStackTrace();
+        } catch (IOException e) {
             System.err.println("Error writing DOT file: " + e.getMessage());
+            e.printStackTrace();
         }
     }
+
 }
